c19e223b3ce03a5a4e53597ed12b4cfbfd8c9dc0
alexander <alexanderperezreginaldo@gmail.com>
2023-03-19 15:52:24 -0500

    actualizando los link de los items de portafolio

--- src/componentes/portafolio/Portafolio.jsx
+++ src/componentes/portafolio/Portafolio.jsx
@@ -7,10 +7,10 @@ const Portafolio = () => {
       <h5>Mis trabajos recientes</h5>
       <h2>Portafolio</h2>
       <div className="container portafolio-container">
-        <ITEM imagen='portafolio1.jpg' nomFoto='proyecto 1' descripcion='PIPATI es un juego de piedra-papel-tijera con un enfoque de fútbol, este proyecto fue desarrollada con html-css-js.' linkGit='https://github.com/talexanderp/JuegoPiedraPapelTijera-html-css-js' linkDemo='https://talexanderp.github.io/JuegoPiedraPapelTijera-html-css-js/src/index' />
-        <ITEM imagen='portafolio2.jpg' nomFoto='proyecto 2' descripcion='Sonqu es una Landing Page con diseño responsive, este proyecto fue desarrollado con html-css-js. Tambien realizamos un diseño UI en Figma antes de inicar el desarrollo.' linkGit='https://github.com/talexanderp/Landing-Mobile---Sonqu' linkDemo='https://talexanderp.github.io/Landing-Mobile---Sonqu/src/index' />
-        <ITEM imagen='portfolio3.jpg' nomFoto='proyecto 3' descripcion='Esta web es una landing desarrollado con Node JS. Tambien consumimos una api para listar videos de un canal de youtube. ' linkGit='https://github.com/talexanderp/proyecto-landing' linkDemo='https://talexanderp.github.io/proyecto-landing/src/index' />
-        <ITEM imagen='portafolio4.jpg' nomFoto='proyecto 4' descripcion='Esta web es un portafolio desarrollado con React de forma responsive.' linkGit='https://github.com/talexanderp/Portafolio_Web-con-React' linkDemo='https://talexanderp.github.io/Portafolio_Web-con-React/' />
+        <ITEM imagen='portafolio1.jpg' nomFoto='proyecto 1' descripcion='PIPATI es un juego de piedra-papel-tijera con un enfoque de fútbol, este proyecto fue desarrollada con html-css-js.' linkGit='https://github.com/luandeer/JuegoPiedraPapelTijera-html-css-js' linkDemo='https://luandeer.github.io/JuegoPiedraPapelTijera-html-css-js/src/index.html' />
+        <ITEM imagen='portafolio2.jpg' nomFoto='proyecto 2' descripcion='Sonqu es una Landing Page con diseño responsive, este proyecto fue desarrollado con html-css-js. Tambien realizamos un diseño UI en Figma antes de inicar el desarrollo.' linkGit='https://github.com/luandeer/Landing-Mobile---Sonqu' linkDemo='https://luandeer.github.io/Landing-Mobile---Sonqu/src/index' />
+        <ITEM imagen='portfolio3.jpg' nomFoto='proyecto 3' descripcion='Esta web es una landing desarrollado con Node JS. Tambien consumimos una api para listar videos de un canal de youtube. ' linkGit='https://github.com/luandeer/proyecto-landing' linkDemo='https://luandeer.github.io/proyecto-landing/src/index.html' />
+        <ITEM imagen='portafolio4.jpg' nomFoto='proyecto 4' descripcion='Esta web es un portafolio desarrollado con React de forma responsive.' linkGit='https://github.com/luandeer/Portafolio_Web-con-React' linkDemo='https://luandeer.github.io/Portafolio_Web-con-React/' />
 
       </div>
     </section>
