cc71a4a46299e43dae46595a813e08a8ead1e399
alexander <alexanderperezreginaldo@gmail.com>
2023-03-21 20:41:56 -0500

    arreglando el footer

--- src/componentes/footer/Footer.jsx
+++ src/componentes/footer/Footer.jsx
@@ -4,18 +4,12 @@ import {FaFacebookF} from 'react-icons/fa'
 import {AiFillInstagram} from 'react-icons/ai'
 import {BsLinkedin} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
-
+import Logo from '../../assets/img/LogoTelmo-v2.png'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer-logo'>Telmo Perez</a>
-      <ul className='permalinks'>
-          <li><a href="#">Inicio</a></li>
-          <li><a href="#about" >Acerca de mi</a></li>
-          <li><a href="#tecnology" >Skills</a></li>
-          <li><a href="#portafolio" >Portafolio</a></li>
-          <li><a href="#contact">Contacto</a></li>
-      </ul>
+      <a href="#" > <img src={Logo} alt="mi logo" /></a>
+      <h1 className='footer-nombre'>Telmo Perez</h1>
       <div className="footer-social">
         <a href="https:/facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
         <a href="https:/instagram.com" target="_blank" rel="noreferrer"><AiFillInstagram /></a>
@@ -24,7 +18,7 @@ const Footer = () => {
       </div>
 
       <div className="footer-copyright">
-        <small>&copy; Luander. Todos los derechos son reservados</small>
+        <small>&copy; Telmo Perez. Todos los derechos son reservados</small>
       </div>
     </footer>
   )
